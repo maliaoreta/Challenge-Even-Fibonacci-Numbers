b7c7beb749d739c5dcecd4e95779e2c5f296b92f
maliaoreta <maliaoreta@gmail.com>
2016-03-17 12:19:10 -1000

    passing initial 2 tests

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -5,10 +5,34 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
+
+
+// when given 13--  1, 2, 3, 5, 8, 13 --- with adding only even numbers -- 2, 8 --> 10
+// if 13 is the highest value
+// begin with 1, and 2
+// add first two target numbers, the next number in sequence is equal to the sum
+// target numbers become the second target number from the prior pair, and the sum 
+
+// 34 -- 1, 2, 3, 5, 8, 13, 21, 34 --> 44
+
+
 function _sumFibs( maxFibValue ) {
   var sum = 0;
+  var firstNum = 1;
+  var secondNum = 2;
+  var sumInSequence = 0;
 
-  // do your work here
+  while (secondNum <= maxFibValue){
+    sumInSequence = firstNum + secondNum;
+
+    firstNum = secondNum;
+    secondNum = sumInSequence;
+
+    if (firstNum % 2 === 0){
+      sum += firstNum;
+    }  
+  }
+
 
   return sum;
 }
@@ -17,10 +41,7 @@ function _sumFibs( maxFibValue ) {
 function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
-  //define your base case, validate your input
 
-
-  //do your work here
 
   return highest;
 };
